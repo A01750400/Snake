17d3a26805e7d5c1d48b3b429ede822c4d340eb9
J4viii3r <73722244+J4viii3r@users.noreply.github.com>
2021-05-07 06:48:01 +0000

    comentarios

--- snake.py
+++ snake.py
@@ -1,7 +1,9 @@
+#Librerías
 from turtle import *
 from random import randrange
 from freegames import square, vector
 
+#Se establecen las localizaciones inciales de los objetos del juego
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
@@ -44,13 +46,13 @@ def move():
     else:
         #Si la cabeza no está tocando la comida, la comida se mueve un segmento en dirección aleatoria
         snake.pop(0)
-        xfood=food.x
+        xfood=food.x        #Se guardan los valores anteriores de la posición de la comida
         yfood=food.y
-        if -200 < food.x < 190 and -200 < food.y < 190:
-            food.x = randrange(xfood-10, xfood+10)
+        if -200 < food.x < 190 and -200 < food.y < 190:     #Se verifica si la comida sigue dentro de los limites, si sí se cambia de lugar
+            food.x = randrange(xfood-10, xfood+10)  #El cambio de posición se hace tomando en cuenta las posiciones anteriores de la comida
             food.y = randrange(yfood-10, yfood+10)
-
-        else:
+        #
+        else:              #Si la comida está fuera de los límites, se regresa al centro de la pantalla
             food.x=0
             food.y=0
 
@@ -63,17 +65,16 @@ def move():
     update()
     ontimer(move, 100)
 
+
 setup(420, 420, 370, 0)
-#Se asignan colores aleatorios a la serpiente y a la comida.
+#Se asignan colores aleatorios a la serpiente y a la comida. Se guardan en variables los resultados de la función para obtener un color aleatorio.
 colorSnake = change_color()
 colorFood = change_color()
-#Se declaran los valores iniciales de la comida.
-food.x=0
-food.y=0
+#Se esconden elementos inecesarios como la tortuga y el tracer
 hideturtle()
 tracer(False)
-listen()
 #Chechar los inputs de las flechas.
+listen()
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
